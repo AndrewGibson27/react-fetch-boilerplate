16b1feaeae162ca87b49cc3c0ec82d47e4de0fb1
Andrew Gibson <atgibson27@gmail.com>
2016-04-07 13:59:19 -0400

    reorder stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import styles from '../scss/test.scss';
+import styles from '../scss/test.scss';
 
 const picture = require('../../assets/img/picture.png');
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className={styles.test}>
                 <img src={picture} />
             </div>
         );
