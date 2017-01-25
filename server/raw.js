f50d1c5452c7be98f247f4849b80e0b222be8920
Andrew Gibson <atgibson27@gmail.com>
2017-01-25 00:21:54 -0600

    use ES6 syntax and babel-register

--- server/raw.js
+++ server/raw.js
@@ -1,12 +1,15 @@
-const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
-const match = require('react-router').match;
-const RouterContext = require('react-router').RouterContext;
-const express = require('express');
-const path = require('path');
-const pug = require('pug');
+import 'babel-register';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import express from 'express';
+import path from 'path';
+import pug from 'pug';
 
 const app = express();
+import App from '../src/components/app/App';
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
+
+console.log(renderToString(<App />));
