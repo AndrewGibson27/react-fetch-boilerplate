bf078338e2d2a183ea3e178064bf4a6fdcd99f0d
Andrew Gibson <atgibson27@gmail.com>
2017-01-25 23:10:12 -0600

    get hot middleware up and running

--- server/raw.js
+++ server/raw.js
@@ -5,11 +5,26 @@ import { match, RouterContext } from 'react-router';
 import express from 'express';
 import path from 'path';
 import pug from 'pug';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.dev.js';
 
-const app = express();
 import App from '../src/components/app/App';
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'pug');
+const compiler = webpack(webpackConfig);
+const app = express();
 
-console.log(renderToString(<App />));
+app.use(express.static('public'));
+app.use(require('webpack-hot-middleware')(compiler));
+app.set('view engine', 'pug');
+app.use(require('webpack-dev-middleware')(compiler, {
+	publicPath: webpackConfig.output.publicPath
+}));
+
+app.listen(3000, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return false;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
