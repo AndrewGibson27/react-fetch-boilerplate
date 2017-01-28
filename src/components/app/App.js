dff9fb77703304d4e62b42dabc80b373cd212b6a
Andrew Gibson <atgibson27@gmail.com>
2017-01-28 15:07:19 -0600

    set up components for base example w/ react-router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
-			<div>Hello world</div>
+			<div>
+				<ul>
+					<li><Link to="/inner">Inner</Link></li>
+				</ul>
+
+				{this.props.children}
+			</div>
 		);
 	}
 }
---
+++ src/components/inner/Inner.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import InnerSpecific from '../innerspecific/InnerSpecific';
+
+export default class Inner extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+        <h2>Inbox</h2>
+
+				<ul>
+					<li><Link to="/inner/309">309</Link></li>
+				</ul>
+
+        {this.props.children}
+      </div>
+		);
+	}
+}
