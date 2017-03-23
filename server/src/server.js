61ec996392c9fe9201c318a2eaca93921d25ab4d
Andrew Gibson <atgibson27@gmail.com>
2017-03-23 01:08:04 -0500

    convert tabs to spaces

--- server/src/server.js
+++ server/src/server.js
@@ -24,53 +24,53 @@ app.set('port', port);
 API.createAPICalls();
 
 if (isDev) {
-	app.use(require('webpack-hot-middleware')(compiler));
-	app.use(require('webpack-dev-middleware')(compiler, {
-		noInfo: true,
-		publicPath: webpackConfig.output.publicPath
-	}));
+  app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
 }
 
 // http://stackoverflow.com/questions/13442377/redirect-all-trailing-slashes-globally-in-express
 app.use((req, res, next) => {
-	if (req.url.substr(-1) == '/' && req.url.length > 1) {
-		res.redirect(301, req.url.slice(0, -1));
-	} else {
-		next();
-	}
+  if (req.url.substr(-1) == '/' && req.url.length > 1) {
+    res.redirect(301, req.url.slice(0, -1));
+  } else {
+    next();
+  }
 });
 
 app.get('*', function(req, res) {
-	match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-		if (renderProps) {
-			request(`${location}/api/list`, function(err, response, body) {
-				if (!err) {
-					res.render('index', {
-						initial: JSON.parse(body),
-						content: renderToStaticMarkup(
-							<RouterContext
-								{...renderProps}
-								createElement={(Component, props) => <Component {...props} initial={JSON.parse(body)} />}
-							/>
-						)
-					});
-				}
-			});
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (renderProps) {
+      request(`${location}/api/list`, function(err, response, body) {
+        if (!err) {
+          res.render('index', {
+            initial: JSON.parse(body),
+            content: renderToStaticMarkup(
+              <RouterContext
+                {...renderProps}
+                createElement={(Component, props) => <Component {...props} initial={JSON.parse(body)} />}
+              />
+            )
+          });
+        }
+      });
 
-		} else {
-			res.render('index', {
-				content: 'Oops, something went wrong'
-		  });
+    } else {
+      res.render('index', {
+        content: 'Oops, something went wrong'
+      });
     }
   });
 });
 
 app.listen(port, 'localhost', function(err) {
-	if (err) {
-		if (isDev) {
-			console.log(err);
-		}
-		return false;
-	}
-	console.log(`Listening at http://localhost:${port}`);
+  if (err) {
+    if (isDev) {
+      console.log(err);
+    }
+    return false;
+  }
+  console.log(`Listening at http://localhost:${port}`);
 });
